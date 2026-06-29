520b0bf304494c850f89bb4fb111e1647432a316
akerimo <abdulkerimseid99@gmail.com>
2026-06-29 21:16:53 +0300

    Intial Project Setup

---
+++ SimpleBankingApplication.java
@@ -0,0 +1,18 @@
+package lab.project;
+
+/*
+This is Java lab project, Which implements OOP using Java. with GUI and Exception handling. Also Database
+Project Title: Simple Banking Application
+              1. user can create account
+              2. user can make deposit
+              3. user can make WithDrawal
+              4. user can transfer funds
+              5. user can see their balance
+              6. user can see their full statements
+              7. user can make currency converter
+*/
+
+
+public class SimpleBankingApplication {
+    
+}
